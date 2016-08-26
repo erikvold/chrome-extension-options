a9faeb853e69f37ee4c9d0d88a59d68367bb36a4
Mike McCabe <mrmikemccabe@gmail.com>
2016-08-27 00:06:05 +0100

    Now successfully serves a react component

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+'use strict'
+
+var React = require('react')
+var ReactDOM = require('react-dom')
+var Hello = require('./Hello.jsx')
+
+ReactDOM.render(<Hello />, document.getElementById('content'))
+
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require("path");
+
+module.exports = {
+	entry: './src/index.jsx',
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: 'index.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.jsx$/,
+				loader: 'jsx-loader'
+			}
+		]
+	},
+	externals: {
+		'react': 'React',
+		'react-dom': 'ReactDOM'
+	},
+	resolve: {
+		extensions: ['', '*.js', '*.jsx']
+	}
+};
