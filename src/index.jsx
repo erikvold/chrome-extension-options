e3633a79518c3e0829ef473af319a43ec5795144
Mike McCabe <mrmikemccabe@gmail.com>
2016-08-31 08:27:50 +0100

    Add options.json

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,9 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var StringOption = require('./StringOption');
-var options = require('./options.json');
+var optionsJSON = require('./options.json');
+
+
 
 ReactDOM.render(<StringOption id={options[0].id} name={options[0].name} />, document.getElementById('content'));
 
