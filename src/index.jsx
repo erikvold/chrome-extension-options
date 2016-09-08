8c31bb94e8d73fac0df3fa4296140150d9a57b47
Mike McCabe <mike.mccabe@bodykind.com>
2016-09-08 17:26:14 +0100

    YAML can be imported in the es6 style

--- src/index.jsx
+++ src/index.jsx
@@ -4,8 +4,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import OptionGroup from './OptionGroup';
 import optionsJSON from './options.json';
-var optionsYAML = require("json!yaml!./options.yaml");
+import optionsYAML from 'json!yaml!./options.yaml';
 
-console.log(optionsYAML);
-
-	ReactDOM.render(<OptionGroup id="option-group" options={optionsYAML} />, document.getElementById('content'));
+ReactDOM.render(<OptionGroup id="option-group" options={optionsYAML} />, document.getElementById('content'));
