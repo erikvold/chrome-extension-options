357d4d84d99efdd5a91e0e471690263d784b8543
Mike McCabe <mrmikemccabe@gmail.com>
2016-09-12 19:42:00 +0100

    Add a title to the Options component

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -6,12 +6,13 @@ import OptionGroup from './OptionGroup';
 export default React.createClass({
 	render: function() {
 
-		var optionGroups = this.props.options.options.groups.map( (group, index) => {
+		var optionGroups = this.props.data.options.groups.map( (group, index) => {
 			const key = "option-group-" + index;
 			return (<OptionGroup key={key} title={group.title} options={group.options} description={group.description} />)
 		});
 		return (
 			<div className="options">
+				<h1>{this.props.data.title}</h1>
 				{optionGroups}
 			</div>
 		);
--- src/index.jsx
+++ src/index.jsx
@@ -6,4 +6,4 @@ import Options from './components/Options';
 import optionsJSON from './options.json';
 import optionsYAML from 'json!yaml!./options.yaml';
 
-ReactDOM.render(<Options id="option-group" options={optionsYAML} />, document.getElementById('content'));
+ReactDOM.render(<Options id="option-group" data={optionsYAML} />, document.getElementById('content'));
